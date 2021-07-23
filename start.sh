c473370c729bb7684c026a4ab5eb4084b55d4bc1
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-07-23 16:27:39 +0300

    Move .env file path to the global variable

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ pushd . > /dev/null
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export DIR
 COMPOSE_DIR="$DIR/compose-files"
+export ENV_FILE_DIR=$COMPOSE_DIR/../.env
 
 # Default props
 export EXPOSE_IP=127.0.0.1
