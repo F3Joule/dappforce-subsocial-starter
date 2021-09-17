b81755e57fec934f2add5ec1d774eb02b8da5e28
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-09-17 20:13:29 +0300

    Update bundle.json and indexer run script

--- hydra-indexer/run.sh
+++ hydra-indexer/run.sh
@@ -6,6 +6,9 @@ pushd . > /dev/null
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
+VAR=$(< .env grep PROJECT_NAME)
+export PROJECT_NAME=${VAR##*=}
+
 if [[ ! -f ".env" ]]; then
   echo "Error: .env file is missing in hydra-indexer directory"
   exit 1
@@ -14,11 +17,11 @@ fi
 DOWN_FLAGS="down"
 if [[ $1 == "--stop" ]]; then
   [[ $2 == "--clean" ]] && DOWN_FLAGS+=" -v"
-  docker-compose -p hydra $DOWN_FLAGS
+  docker-compose -p "$PROJECT_NAME" $DOWN_FLAGS
   exit 0
 fi
 
 echo "Running docker-compose of Hydra Indexer for Subsocial..."
-docker-compose -p hydra up -d
+docker-compose -p "$PROJECT_NAME" up -d
 
 popd > /dev/null
